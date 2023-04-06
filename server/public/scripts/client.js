8dee5b071174ab0bbf03fd5e513ef47b49a7aea5
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-06 14:43:55 -0500

    Built out submitGuesses function
    -function creates a newGuesses array of objects.
    	-Each object is an individual players guess in the format of following
    	  example. {name:'Player 1', guess: $('#Player1').val()}
    	-Then clears input field for Player 1.
    -function then sends a POST to the server on url /playerGuesses with the data of the newGuesses array.
    
    -Confirmed that req is being received by the server.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,8 +6,11 @@ function handleReady() {
   console.log("jquery is loaded!")
   $('#submit-Player-Count').on('click', submitPlayers)
 
+  $('#Guess-Input').on('click', '#Submit-Players-Guess', submitGuesses)
+
 }
 
+// Function to create a new input field per player of the game.
 function submitPlayers(event) {
   // Prevent reload of page on submit of form
   event.preventDefault();
@@ -15,7 +18,7 @@ function submitPlayers(event) {
 
   $('#Guess-Input').empty('');
 
-
+  // Create a new guess input for each player
   for (let i = 1; i <= playerCount; i++) {
     $('#Guess-Input').append(`
         <h3 class="row">Player ${i}
@@ -24,10 +27,53 @@ function submitPlayers(event) {
     `)
   }
 
+  $('#Player-Count').val('');
+
+  // Create 1 submit button for all player's guesses
   $('#Guess-Input').append(`
     <div>
       <button id="Submit-Players-Guess">Submit Guesses</button>
     </div>
   `)
+}// end submitPlayers
 
+
+function submitGuesses(event) {
+  // Prevent reload of page on submit of form
+  event.preventDefault();
+  console.log('in submitGuesses');
+
+  //loop through player guesses add to newGuesses Array
+    //Guess = {name: `Player ${i})`, guess: $('#Player ${i}').val()}
+  
+  let newGuesses = [];
+
+  for (let i = 1; i <= playerCount; i++) {
+    let playerGuess = {
+      name: `Player ${i}`,
+      guess: $(`#Player${i}`).val()
+    }
+    
+    newGuesses.push(playerGuess);
+
+    $(`#Player${i}`).val('')
+  }
+
+  console.log(newGuesses);
+  $.ajax({
+    method: 'POST',
+    url: '/playerGuesses',
+    data: newGuesses
+  }).then(function(response){
+    // Will need to build out function to bring DOM into sync
+    getGuesses();
+  }).catch(function(error){
+    alert('issues with POST, try again later!')
+  })
+
+}// end submitGuesses
+
+
+function getGuesses() {
+  console.log('in getGuesses');
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+let guessArray = [];
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,6 +12,20 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+app.get('/playerGuesses', (req, res) => {
+  console.log('GET /playerGuesses');
+
+})
+
+app.post('/playerGuesses', (req, res) => {
+  console.log('POST /playerGuesses');
+
+  let newGuesses = req.body;
+  guessArray.push(newGuesses)
+
+  res.sendStatus(201)
+})
+
 
 
 app.listen(PORT, () => {
