1776c06136c1e89edc9f0916e3be9a4099bc699b
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-06 13:41:20 -0500

    Built out submitPlayers function
    -submitPlayers takes in the # of players from the first input field and creates a new guess input for each player.
    -There is then 1 submit button that is created for all players

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,33 @@
 $(document).ready(handleReady);
 
+let playerCount = 1;
+
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#submit-Player-Count').on('click', submitPlayers)
+
+}
+
+function submitPlayers(event) {
+  // Prevent reload of page on submit of form
+  event.preventDefault();
+  playerCount = $('#Player-Count').val();
+
+  $('#Guess-Input').empty('');
+
+
+  for (let i = 1; i <= playerCount; i++) {
+    $('#Guess-Input').append(`
+        <h3 class="row">Player ${i}
+          <input id="Player${i}" placeholder="Guess">
+        </h3>
+    `)
+  }
+
+  $('#Guess-Input').append(`
+    <div>
+      <button id="Submit-Players-Guess">Submit Guesses</button>
+    </div>
+  `)
+
 }
